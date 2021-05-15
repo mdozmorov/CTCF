8f364febd5ec231fcbdefcb8811a6c8a994a70b0
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2021-05-14 20:43:21 -0400

    Create dummy documentation to get rid of Warning: no parsed files found

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' Hello, world!
+#'
+#' This is an example function named 'hello'
+#' which prints 'Hello, world!'.
+#'
+#' @export
+#' @return Print Hello, world!
+
+hello <- function() {
+  print("Hello, world!")
+}
