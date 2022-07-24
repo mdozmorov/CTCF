69dec7029817e299ec699f02de147899e61870b2
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2022-07-24 15:16:11 -0400

    Add object annotations

---
+++ R/zzz.R
@@ -0,0 +1,503 @@
+#' T2T.CIS_BP_2.00_Homo_sapiens
+#'
+#' T2T CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.CTCFBSDB_PWM
+#'
+#' T2T CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' T2T CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' T2T CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.Jolma2013
+#'
+#' T2T CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.MA0139.1
+#'
+#' T2T CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' T2T.SwissRegulon_human_and_mouse
+#'
+#' T2T CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name T2T.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.CIS_BP_2.00_Homo_sapiens
+#'
+#' hg38 CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.CTCFBSDB.CTCF_predicted_human
+#'
+#' hg38 CTCF predicted motifs from https://insulatordb.uthsc.edu/. liftOver from hg18
+#'
+#' @name hg38.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.CTCFBSDB_PWM
+#'
+#' hg38 CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' hg38 CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' hg38 CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.Jolma2013
+#'
+#' hg38 CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.MA0139.1
+#'
+#' hg38 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.SCREEN.GRCh38_CTCF
+#'
+#' hg38 CTCF-bound human cis-regulatory elements from https://screen.encodeproject.org/.
+#' Derived from experimentally obtained data.
+#'
+#' @name hg38.SCREEN.GRCh38_CTCF
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg38.SwissRegulon_human_and_mouse
+#'
+#' hg38 CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg38.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.CIS_BP_2.00_Homo_sapiens
+#'
+#' hg19 CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.CTCFBSDB.CTCF_predicted_human
+#'
+#' hg19 CTCF predicted motifs from https://insulatordb.uthsc.edu/. liftOver from hg18
+#'
+#' @name hg19.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.CTCFBSDB_PWM
+#'
+#' hg19 CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' hg19 CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' hg19 CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.Jolma2013
+#'
+#' hg19 CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.MA0139.1
+#'
+#' hg19 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg19.SwissRegulon_human_and_mouse
+#'
+#' hg19 CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg19.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg18.CTCFBSDB.CTCF_predicted_human
+#'
+#' hg18 CTCF predicted motifs from https://insulatordb.uthsc.edu/.
+#'
+#' @name hg18.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' hg18.MA0139.1
+#'
+#' hg18 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name hg18.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.CIS_BP_2.00_Homo_sapiens
+#'
+#' mm39 CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.CTCFBSDB_PWM
+#'
+#' mm39 CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' mm39 CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' mm39 CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.Jolma2013
+#'
+#' mm39 CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.MA0139.1
+#'
+#' mm39 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm39.SwissRegulon_human_and_mouse
+#'
+#' mm39 CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm39.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.CIS_BP_2.00_Homo_sapiens
+#'
+#' mm10 CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.CTCFBSDB.CTCF_predicted_human
+#'
+#' mm10 CTCF predicted motifs from https://insulatordb.uthsc.edu/. liftOver from mm8
+#'
+#' @name mm10.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.CTCFBSDB_PWM
+#'
+#' mm10 CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' mm10 CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' mm10 CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.Jolma2013
+#'
+#' mm10 CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.MA0139.1
+#'
+#' mm10 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.SCREEN.GRCh38_CTCF
+#'
+#' mm10 CTCF-bound human cis-regulatory elements from https://screen.encodeproject.org/.
+#' Derived from experimentally obtained data.
+#'
+#' @name mm10.SCREEN.GRCh38_CTCF
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm10.SwissRegulon_human_and_mouse
+#'
+#' mm10 CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm10.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.CIS_BP_2.00_Homo_sapiens
+#'
+#' mm9 CTCF motifs detected using human PWM matrices from http://cisbp.ccbr.utoronto.ca/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.CIS_BP_2.00_Homo_sapiens
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.CTCFBSDB.CTCF_predicted_human
+#'
+#' mm9 CTCF predicted motifs from https://insulatordb.uthsc.edu/. liftOver from mm8
+#'
+#' @name mm9.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.CTCFBSDB_PWM
+#'
+#' mm9 CTCF motifs detected using PWM matrices from https://insulatordb.uthsc.edu/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.CTCFBSDB_PWM
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#'
+#' mm9 CTCF motifs detected using human PWM matrices from https://hocomoco11.autosome.org/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.HOCOMOCOv11_core_HUMAN_mono_meme_format
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#'
+#' mm9 CTCF motifs detected using human PWM matrices from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.JASPAR2022_CORE_vertebrates_non_redundant_v2
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.Jolma2013
+#'
+#' mm9 CTCF motifs detected using PWM matrices from DOI:10.1016/j.cell.2012.12.009,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.Jolma2013
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.MA0139.1
+#'
+#' mm9 CTCF motifs detected using MA0139.1 PWM matrix from https://jaspar.genereg.net/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.MA0139.1
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm9.SwissRegulon_human_and_mouse
+#'
+#' mm9 CTCF motifs detected using PWM matrices from https://swissregulon.unibas.ch/sr/,
+#' by FIMO. Filtering by p-value < 1e-6 and reducing GRanges is recommended.
+#'
+#' @name mm9.SwissRegulon_human_and_mouse
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
+
+#' mm8.CTCFBSDB.CTCF_predicted_human
+#'
+#' mm8 CTCF predicted motifs from https://insulatordb.uthsc.edu/.
+#'
+#' @name mm8.CTCFBSDB.CTCF_predicted_human
+#' @docType data
+#' @author Mikhail Dozmorov \email{mikhail.dozmorov@gmail.com}
+NULL
